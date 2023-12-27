71c77335e65d97ec821cc388a4a670250031deb1
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-12-27 16:21:23 +0100

    update: home page

---
+++ components/header.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+
+export const Header = () => {
+    return (
+        <header className="container py-4">
+            <Link href="/">
+                R
+            </Link>
+        </header>
+    )
+}
