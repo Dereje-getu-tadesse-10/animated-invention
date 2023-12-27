b27d21d5e6ca363b125588b4709e57942eda18c2
Dereje-getu-tadesse-10 <getutadessedereje@icloud.com>
2023-12-27 16:59:40 +0100

    update: shopify custom line characters limit

--- stores/useLineItem.tsx
+++ stores/useLineItem.tsx
@@ -5,13 +5,17 @@ type FieldType = "Text court" | "Text long" | "Checkbox"
 type LineItem = {
     fiedlType: FieldType,
     label: string;
+    charactersLimit: number,
     setFiedlType: (fiedlType: FieldType) => void
     setLabel: (label: string) => void,
+    setcharactersLimit: (limit: number) => void
 }
 
 export const useLineTime =  create<LineItem>((set)=>({
  fiedlType: "Text court",
  label: "Your label",
+ charactersLimit: 0,
  setFiedlType: (fiedlType) => set(() => ({ fiedlType: fiedlType })),
  setLabel: (label) => set(() => ({ label: label})),
+ setcharactersLimit: (limit) => set(()=>({ charactersLimit: limit }))
 }))
